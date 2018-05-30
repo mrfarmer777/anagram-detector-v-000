6de35d7f2884ad8b781f775a4de2b16fd0545c81
Matt Farmer <mrfarmer777@gmail.com>
2018-05-30 02:28:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     anarray.select {|pot_ana| self.is_anagram?(word,pot_ana)}
   end
 
-  def is_anagram?(word,pot_ana)
+  def is_anagram?(word,pot_ana) #seems like this should be a 'private' method, but maybe not.
     word_array=word.split(//).sort
     ana_array=pot_ana.split(//).sort
     word_array==ana_array
