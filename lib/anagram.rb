d04e0ffa399eacea83db417663bcc72883a4df8e
Matt Farmer <mrfarmer777@gmail.com>
2018-05-30 02:24:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(anarray)
+    anarray.collect {|pot_ana| self.is_anagram?(word,pot_ana)}
+  end
+
+  private 
+  
+  def is_anagram(word,pot_ana)
+    word_array=word.split.sort
+    ana_array=pot_ana.split.sort
+    word_array==ana_array
+  end
+  
+
+    
